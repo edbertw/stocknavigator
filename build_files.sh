0419baa838f5642d6d6ac6416c5ba75aefbdcd6f
edbertw <edbertw@connect.hku.hk>
2025-05-23 03:46:16 +0800

    fix deploy

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-python3.11 -m venv venv
-source venv/bin/activate
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 cd mybackend
-python3 manage.py collectstatic
+python manage.py collectstatic
--- mybackend/mybackend/settings.py
+++ mybackend/mybackend/settings.py
@@ -76,17 +76,17 @@ WSGI_APPLICATION = 'mybackend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 '''
+
 DATABASES = {
-    
+    '''
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
@@ -95,8 +95,9 @@ DATABASES = {
         'HOST': "containers-us-west-196.railway.app',
         'PORT': '6625',
     }
+    '''
 }
-'''
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
