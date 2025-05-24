809fe8b1ec680cebd0e1219c27220cd2f6c0c38f
edbertw <edbertw@connect.hku.hk>
2025-05-24 16:52:59 +0700

    fix deploy

--- build_files.sh
+++ build_files.sh
@@ -3,4 +3,4 @@ cd mybackend
 python3.11 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
-python3.12 manage.py collectstatic --noinput 
+python3 manage.py collectstatic --noinput 
