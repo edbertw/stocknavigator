51217919a4b5318cf1c082692b664b72f6146be5
edbertw <edbertw@connect.hku.hk>
2025-07-02 10:49:14 +0800

    Fix paths

--- server/api/sentiment.py
+++ server/api/sentiment.py
@@ -19,11 +19,11 @@ import os
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
-model_path = os.path.join(os.path.dirname(__file__), "api/sentiment_rnn.pt")
+model_path = os.path.join(os.path.dirname(__file__), "sentiment_rnn.pt")
 model = torch.jit.load(model_path, map_location=torch.device('cpu'))
 
 # Load the dataset
-csv_path = os.path.join(os.path.dirname(__file__), "api/data.csv")  # Relative to the script
+csv_path = os.path.join(os.path.dirname(__file__), "data.csv")  
 data = pd.read_csv(csv_path)
 #print(data.head())
 
